abd091db5ad986ec6464ae1e9b468c1344e54ee4
tarikondabahuddin <140080638+tarikondabahuddin@users.noreply.github.com>
2024-02-07 15:18:49 +0530

    Create guessthenumber.py

---
+++ guessthenumber.py
@@ -0,0 +1,15 @@
+import random
+random_number=random.randrange(0, 10)
+print(random_number)
+print("please ensure you have only three chances to guess the number")
+for i in range(1,4):
+    k=int(input("enter the number "+" "+str(i)+" : "))
+    if(k>int(random_number)):
+        print("greater number")
+    elif(k<int(random_number)):
+        print("lesser number")
+    elif(k==int(random_number)):
+        print("you won the game.")
+        break
+if(k!=int(random_number)):
+    print("you lost")
